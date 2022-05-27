ac5b09b5322df245fbf7c816a49eb76ed24b964b
Antuan <58564862+Antuan01@users.noreply.github.com>
2022-05-26 22:35:37 -0400

    Development (#2)
    * app start
    
    * added requirements.txt
    
    * added test routes

--- app.py
+++ app.py
@@ -1,14 +1,37 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask import request
 
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
+@app.route("/api/random")
+def random_quote():
+    return { "quote":"naosehusnoahunsoehsun"}
 
-api.add_resource(HelloWorld, '/')
+@app.route("/api/<author>/quote")
+def quote_from_author(author):
+    res = "quote from: " + author
+    return { "quote": res }
+
+@app.route("/api/authors", methods=["GET", "POST"])
+def handle_authors():
+    if request.method == "POST":
+        return { "authors":["list"] }
+    else:
+        return { "data": "author created" }
+
+@app.route("/api/quote/<int:author_id>", methods=["POST"])
+def create_quote(author_id):
+    return {"data":"created"}
+
+@app.route("/test")
+def hello_world():
+    return {'test': 'tset'}
+
+@app.route("/about")
+def about():
+    return { "data": "this is about" }
 
 if __name__ == '__main__':
     app.run(debug=True)
